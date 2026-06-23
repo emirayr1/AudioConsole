b8f2965a680b0d8042285ea2d85b6ca7b81e31b3
EmirAyar <230101004@mgu.edu.tr>
2026-06-23 16:45:43 +0300

    Fade Update

--- include/Effect/Fade.h
+++ include/Effect/Fade.h
@@ -18,12 +18,16 @@ public:
 
     void setType(FadeType type) { m_type = type; }
     void setFadeLenghtInSeconds(float length) { fadeLengthInSeconds = length; }
+    void setLengthInSamples(float length, float sampleRate, int numChannels) {lengthInSamples = static_cast<size_t>(((int)sampleRate * numChannels) * length); }
     void setIsFadeIn(bool condition) { isFadeIn = condition; }
     void setIsFadeOut(bool condition) { isFadeOut = condition; }
+
+    std::vector<float> generateFadeArray();
 
 private:
     FadeType m_type = FadeType::None;
     bool isFadeIn = false;
     bool isFadeOut = false;
     float fadeLengthInSeconds = 0.0f;
+    size_t lengthInSamples = 0;
 };
--- src/Effect/Fade.cpp
+++ src/Effect/Fade.cpp
@@ -3,6 +3,25 @@
 void Fade::process(std::vector<float>& audioBuffer)
 {
     if(isFadeIn){
-        
+
     }
+}
+
+std::vector<float> Fade::generateFadeArray()
+{
+    std::vector<float> fadeArray(lengthInSamples);
+    
+    if (fadeArray.size() <= 1) {
+        if (!fadeArray.empty()) fadeArray[0] = 1.0f;
+        return fadeArray;
+    }
+
+    float maxIndex = static_cast<float>(fadeArray.size() - 1);
+
+    for(size_t i = 0; i < fadeArray.size(); i++)
+    {
+        fadeArray[i] = static_cast<float>(i) / maxIndex;
+    }
+
+    return fadeArray;
 }
